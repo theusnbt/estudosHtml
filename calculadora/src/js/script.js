a052697ca1381a08c450e0c42d9ebbc3d6e6935d
Matheus <matheusnalves33@gmail.com>
2026-08-05 17:11:38 -0300

    feat(calculadora): adiciona estrutura inicial da calculadora
    Cria a interface da calculadora com HTML e CSS responsivos.
    Implementa as operações básicas de soma, subtração, multiplicação e divisão, incluindo validação de entradas numéricas e tratamento para divisão por zero.

---
+++ calculadora/src/js/script.js
@@ -0,0 +1,46 @@
+function calcula(op){
+    var resultado;
+
+    let n1 = document.getElementById("box1").value;
+    let n2 = document.getElementById("box2").value;
+
+    if(isNaN(n1)){
+        alert("O valor deve ser apenas números");
+        document.getElementById("box1").value="";
+        document.getElementById("box1").focus();
+    }
+    else if(isNaN(n2)){
+        alert("O valor deve ser apenas números");
+        document.getElementById("box2").value="";
+        document.getElementById("box2").focus();
+    }
+    else{
+        n1 = parseFloat(n1);
+        n2 = parseFloat(n2);
+
+        if(op == "+")
+            resultado = n1 + n2;
+      
+        else if(op == "-")
+            resultado = n1 - n2;
+        
+        else if(op == "*")
+            resultado = n1 * n2;
+        
+        else if(n2 == 0){
+                alert("Não se pode dividir por zero"); 
+                document.getElementById("box2").value = "";
+                document.getElementById("result").value = "";
+                document.getElementById("box2").focus();
+                return;
+        }
+        else
+                resultado = n1/n2;
+       
+        document.getElementById("result").value = resultado;
+    }
+}
+
+function limpar(){
+    document.getElementById("box1").focus();
+}
